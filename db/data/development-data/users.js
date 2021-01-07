74ddce01a21243f1de789fe2002eedcf97f02ceb
Clara Boothby <clarahboothby@gmail.com>
2021-01-07 16:13:39 +0000

    modifying dev-data to store firebase generated ids

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,14 +1,16 @@
 module.exports = [
   {
+    user_id: "vQyKA3FuWdSAxBVs8MX3rKYCefi1",
     username: "stevelad83",
     name: "Steve",
-    email: "stevelad83@gmail.com",
+    email: "be-test-1@test.com",
     location: "(53.4084, 2.9916)",
   },
   {
+    user_id: "knQicRC1k1UGAROHO5HlnSYUIfS2",
     username: "cb95",
     name: "Clara",
-    email: "cb95@gmail.com",
+    email: "be-test-2@test.com",
     location: "(53.4808, 2.2426)",
   },
 ];
